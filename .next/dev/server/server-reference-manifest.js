59cb0023a5b8797ad3f2cbfe8a9adfe554c7342c
Terry Xiao <129630311+txstartup1985@users.noreply.github.com>
2026-04-06 04:05:00 +0000

    Fix node ver issue

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aoZwrbnutoAwI9wJdzUK8RMS4I8+0LPMBsbo9fznGsU=\"\n}"
--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_0p2ml0n.css","static/chunks/node_modules_0zk7ge2._.js","static/chunks/src_0kbecw4._.js"],
-    source: "dynamic"
-});
